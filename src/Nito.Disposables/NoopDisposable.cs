7c89e9e2b6aa80841b54c43d1296c340f8497078
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-08-23 17:33:10 -0400

    feat: add NoopDisposable

---
+++ src/Nito.Disposables/NoopDisposable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nito.Disposables
+{
+    /// <summary>
+    /// A singleton disposable that does nothing when disposed.
+    /// </summary>
+    public sealed class NoopDisposable: IDisposable
+    {
+        private NoopDisposable()
+        {
+        }
+
+        /// <summary>
+        /// Does nothing.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance of <see cref="NoopDisposable"/>.
+        /// </summary>
+        public static NoopDisposable Instance { get; } = new NoopDisposable();
+    }
+}
