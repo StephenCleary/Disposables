491580e5e8da8aedaf96c6c7a79455991a6c117f
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-07-20 07:55:57 -0400

    Add "tests" for NoopDisposable.

---
+++ test/UnitTests/NoopDisposableUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nito.Disposables;
+using System.Linq;
+using Xunit;
+
+namespace UnitTests
+{
+    public class NoopDisposableUnitTests
+    {
+        [Fact]
+        public void Instance_IsSingleton()
+        {
+            Assert.Same(NoopDisposable.Instance, NoopDisposable.Instance);
+        }
+
+        [Fact]
+        public void Dispose_MultipleTimes_DoesNothing()
+        {
+            NoopDisposable.Instance.Dispose();
+            NoopDisposable.Instance.Dispose();
+        }
+    }
+}
