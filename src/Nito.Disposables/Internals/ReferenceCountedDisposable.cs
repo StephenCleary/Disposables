c2e74e28c04d638b53d071d37639941069eea23b
Stephen Cleary <github.ourteddybear@xoxy.net>
2021-12-08 22:26:17 -0500

    Pubternalize.

---
+++ src/Nito.Disposables/Internals/ReferenceCountedDisposable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nito.Disposables.Internals
+{
+    /// <summary>
+    /// An instance that represents a reference count.
+    /// </summary>
+    public sealed class ReferenceCountedDisposable<T> : SingleDisposable<IReferenceCounter<T>>, IReferenceCountedDisposable<T>
+        where T : class, IDisposable
+    {
+        /// <summary>
+        /// Initializes a reference counted disposable that refers to the specified reference count. The specified reference count must have already been incremented for this instance.
+        /// </summary>
+        public ReferenceCountedDisposable(IReferenceCounter<T> referenceCounter)
+            : base(referenceCounter)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void Dispose(IReferenceCounter<T> referenceCounter)
+        {
+            referenceCounter.TryDecrementCount()?.Dispose();
+        }
+
+        T IReferenceCountedDisposable<T>.Target => ReferenceCounter.TryGetTarget() ?? throw new ObjectDisposedException(nameof(ReferenceCountedDisposable<T>));
+
+        IReferenceCountedDisposable<T> IReferenceCountedDisposable<T>.AddReference()
+        {
+            var referenceCounter = ReferenceCounter;
+            if (!referenceCounter.TryIncrementCount())
+                throw new ObjectDisposedException(nameof(ReferenceCountedDisposable<T>));
+            return new ReferenceCountedDisposable<T>(referenceCounter);
+        }
+
+        IWeakReferenceCountedDisposable<T> IReferenceCountedDisposable<T>.AddWeakReference() => new WeakReferenceCountedDisposable<T>(ReferenceCounter);
+
+        private IReferenceCounter<T> ReferenceCounter
+        {
+            get
+            {
+                IReferenceCounter<T> referenceCounter = null!;
+                // Implementation note: IncrementCount always "succeeds" in updating the context since it always returns the same instance.
+                // So, we know that IncrementCount will be called at most once. It may also be called zero times if this instance is disposed.
+                if (!TryUpdateContext(x => referenceCounter = x))
+                    throw new ObjectDisposedException(nameof(ReferenceCountedDisposable<T>));
+                return referenceCounter;
+            }
+        }
+    }
+}
---
+++ src/Nito.Disposables/Internals/WeakReferenceCountedDisposable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nito.Disposables.Internals
+{
+    /// <summary>
+    /// An instance that represents an uncounted weak reference.
+    /// </summary>
+    public sealed class WeakReferenceCountedDisposable<T> : IWeakReferenceCountedDisposable<T>
+        where T : class, IDisposable
+    {
+        private readonly WeakReference<IReferenceCounter<T>> _weakReference;
+
+        /// <summary>
+        /// Creates an instance that weakly references the specified reference counter. The specified reference counter should not be incremented.
+        /// </summary>
+        public WeakReferenceCountedDisposable(IReferenceCounter<T> referenceCounter)
+        {
+            _weakReference = new(referenceCounter);
+        }
+
+        IReferenceCountedDisposable<T>? IWeakReferenceCountedDisposable<T>.TryAddReference()
+        {
+            if (!_weakReference.TryGetTarget(out var referenceCounter))
+                return null;
+            if (!referenceCounter.TryIncrementCount())
+                return null;
+            return new ReferenceCountedDisposable<T>(referenceCounter);
+        }
+
+        T? IWeakReferenceCountedDisposable<T>.TryGetTarget()
+        {
+            if (!_weakReference.TryGetTarget(out var referenceCounter))
+                return null;
+            return referenceCounter.TryGetTarget();
+        }
+    }
+}
